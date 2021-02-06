e0ad87122cbef5aabee2457d588d3143f3a95452
v 1 r t l <pilll.PL22@gmail.com>
2021-02-06 19:13:54 +0300

    Update example.js

--- example.js
+++ example.js
@@ -1,6 +1,8 @@
 import { httx } from './index.js'
 import { handle } from './handle.js'
 import { router } from './router.js'
+import { notFound } from './notFound.js'
+import { find } from './find.js'
 
 const r = router()
 
@@ -8,6 +10,6 @@ r.use('/', (_, next) => next())
 
 r.use('/params', (obj) => void obj.x = 5)
 
-const { server } = httx({ handle, mw: r.routes() })
+const { server } = httx({ handle, mw: r.routes(), find, notFound, formatObj: objFromReq })
 
 server.listen(3000, () => console.log(`Started on :3000`))
